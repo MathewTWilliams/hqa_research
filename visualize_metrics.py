4616454d398297b613596c403d52f446ca68431c
Matt Williams <williams.mathew.t@gmail.com>
2022-12-28 12:46:19 -0500

    Updating visuals be dataset

--- visualize_metrics.py
+++ visualize_metrics.py
@@ -8,21 +8,21 @@ from utils import ACCURACY_OUTPUT_FILE, VISUAL_DIR
 import os
 from utils import RECON_ROOT_NAMES
 
-def show_and_save_graph(recon): 
+def show_and_save_graph(dataset): 
 
     accuracy_df = pd.read_csv(ACCURACY_OUTPUT_FILE, index_col=False)
 
     if not os.path.exists(VISUAL_DIR): 
         os.mkdir(VISUAL_DIR)
 
-    title = f"{recon} Lenet Accuracy Comparison"
+    title = f"{dataset} Lenet Accuracy Comparison"
 
     font_dict = {'family': 'Arial', 
                  'color' : 'darkblue', 
                  'weight' : 'normal', 
                  'size' : 16 }
 
-    cur_df = accuracy_df[accuracy_df["Reconstruction"] == recon]
+    cur_df = accuracy_df[accuracy_df["Dataset"] == dataset]
 
     y_positions = np.arange(len(cur_df.index))
     x_values = []
@@ -30,11 +30,11 @@ def show_and_save_graph(recon):
 
     for _, row_ds in cur_df.iterrows():
         cur_model = row_ds["Model"]
-        cur_dataset = row_ds["Dataset"]
+        cur_recon = row_ds["Reconstruction"]
         cur_attack = row_ds["Attack"]
         x_values.append(row_ds["Average Accuracy"])
 
-        cur_label = f"{cur_model} | {cur_dataset} | {cur_attack}"
+        cur_label = f"{cur_model} | {cur_attack} | {cur_recon}"
         labels.append(cur_label)
 
     x_values = np.array(x_values)
@@ -44,19 +44,20 @@ def show_and_save_graph(recon):
     x_values = x_values[sorted_indexes]
     labels = labels[sorted_indexes]
 
-    plt.figure(figsize=(22,13))
+    plt.figure(figsize=(20,10))
     plt.title(title, fontdict=font_dict)
     plt.xlabel("Accuracy", fontdict=font_dict)
     plt.ylabel("LeNet Configuration", fontdict = font_dict)
     plt.barh(y_positions, x_values, tick_label = labels)
     #plt.xlim([0.5, 1])
-    plt.savefig(os.path.join(VISUAL_DIR, f"{recon}__accuracies.png"))
+    plt.savefig(os.path.join(VISUAL_DIR, f"{dataset}_accuracies.png"))
     plt.clf()
 
 def main():
         
-        for recon in RECON_ROOT_NAMES:
-            show_and_save_graph(recon)
+    datasets = ["MNIST", "Fashion_MNIST", "EMNIST"]
+    for dataset in datasets:
+        show_and_save_graph(dataset)
 
 if __name__ == "__main__": 
     main()
