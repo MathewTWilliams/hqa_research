34c4447dba849d541cfe08d7f41bd7dc36b21104
Matt Williams <williams.mathew.t@gmail.com>
2023-01-04 22:14:03 -0500

    removing old visuals and adding new ones. Updated visualize_metrics script

--- visualize_metrics.py
+++ visualize_metrics.py
@@ -8,57 +8,54 @@ from utils import ACCURACY_OUTPUT_FILE, VISUAL_DIR
 import os
 from utils import RECON_ROOT_NAMES
 
-def show_and_save_graph(dataset): 
+def make_and_save_line_graph(dataset, model_name): 
 
     accuracy_df = pd.read_csv(ACCURACY_OUTPUT_FILE, index_col=False)
 
     if not os.path.exists(VISUAL_DIR): 
         os.mkdir(VISUAL_DIR)
 
-    title = f"{dataset} Lenet Accuracy Comparison"
+    title = f"{model_name} Accuracy Comparison on {dataset}"
 
     font_dict = {'family': 'Arial', 
                  'color' : 'darkblue', 
                  'weight' : 'normal', 
                  'size' : 16 }
 
-    cur_df = accuracy_df[accuracy_df["Dataset"] == dataset]
+    cur_df = accuracy_df[(accuracy_df["Dataset"] == dataset) & \
+                        (accuracy_df["Model"] == model_name)]
 
-    y_positions = np.arange(len(cur_df.index))
-    x_values = []
-    labels = []
+
+
+    reg_y_values = []
+    atk_y_values = []
+    x_ticks = []
 
     for _, row_ds in cur_df.iterrows():
-        cur_model = row_ds["Model"]
         cur_recon = row_ds["Reconstruction"]
         cur_attack = row_ds["Attack"]
-        x_values.append(row_ds["Average Accuracy"])
-
-        cur_label = f"{cur_model} | {cur_attack} | {cur_recon}"
-        labels.append(cur_label)
-
-    x_values = np.array(x_values)
-    labels = np.array(labels)
-
-    sorted_indexes = np.argsort(x_values)
-    x_values = x_values[sorted_indexes]
-    labels = labels[sorted_indexes]
+        if cur_attack == "None":
+            reg_y_values.append(row_ds["Average Accuracy"])
+            x_ticks.append(cur_recon)
+        else:
+            atk_y_values.append(row_ds["Average Accuracy"])
 
     plt.figure(figsize=(20,10))
     plt.title(title, fontdict=font_dict)
-    plt.xlabel("Accuracy", fontdict=font_dict)
-    plt.ylabel("LeNet Configuration", fontdict = font_dict)
-    plt.barh(y_positions, x_values, tick_label = labels)
-    #plt.xlim([0.5, 1])
-    plt.savefig(os.path.join(VISUAL_DIR, f"{dataset}_accuracies.png"))
+    plt.xlabel("Reconstruction Layer", fontdict=font_dict)
+    plt.ylabel("Accuracy", fontdict=font_dict)
+    plt.plot(x_ticks, reg_y_values, label = "Regular")
+    plt.plot(x_ticks, atk_y_values, label = "FGSM Attack")
+    plt.legend()
+    plt.savefig(os.path.join(VISUAL_DIR, f"{model_name}_{dataset}_accuracies.png"))
     plt.clf()
 
 def main():
         
-    datasets = ["MNIST", "Fashion_MNIST", "EMNIST", "MNIST_GELU"]
-    #datasets = ["Tiled_MNIST", "Tiled_Fashion_MNIST", "Tiled_EMNIST"]
+    datasets = ["MNIST", "Fashion_MNIST", "EMNIST", "MNIST_GELU",
+                "Tiled_MNIST", "Tiled_Fashion_MNIST", "Tiled_EMNIST"]
     for dataset in datasets:
-        show_and_save_graph(dataset)
+        make_and_save_line_graph(dataset, "Lenet")
 
 if __name__ == "__main__": 
     main()
