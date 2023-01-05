ac3ec47a1d04352d7dc4e25b161f71f26d515530
Matt Williams <williams.mathew.t@gmail.com>
2023-01-04 21:35:06 -0500

    Updating more visuals. Combined TSNE visualizations into the evaluate recons script.

--- evaluate_recons.py
+++ evaluate_recons.py
@@ -1,11 +1,9 @@
 # Author: Matt Williams
 # Version: 12/26/2022
 
-import pandas as pd
 import numpy as np
 from utils import *
-from torch.utils.data import DataLoader
-from torchvision import transforms
+from torch.utils.data import DataLoader, Subset
 import os
 import torch
 from sklearn.metrics import confusion_matrix
@@ -14,70 +12,100 @@ from tqdm import tqdm
 from numeric_image_folder import NumericImageFolder
 from slice_torch_dataset import CombinedDataSet
 from pytorch_cnn_base import run_predictions
+from proposal_visuals import run_tsne
+from sklearn.model_selection import train_test_split
 
 def evaluate_dataset(model_name, test_labels, predictions, ds_name, recon_name, attack = None):
-    diag_accuracies = confusion_matrix(test_labels, predictions, normalize="true").diagonal().tolist()
-    avg_accuracy = np.sum(diag_accuracies) / len(diag_accuracies)
+    diag_counts = confusion_matrix(test_labels, predictions).diagonal().tolist()
+    num_correct = np.sum(diag_counts)
+    avg_accuracy = num_correct / len(test_labels)
     attack_name = attack if attack != None else "None"
     add_accuracy_results(model_name, ds_name, recon_name, attack_name, avg_accuracy)
 
+    return len(test_labels) - num_correct
 
-def eval_model(model_save_path, model_name, dataset):
 
-    lenet_model = torch.load(model_save_path)
-    lenet_model.eval()
+def eval_model(model_save_path, model_name, dataset, num_classes):
 
     ds_name = dataset.split("\\")[-1]
 
     for root in tqdm(RECON_ROOT_NAMES):
+        lenet_model = torch.load(model_save_path)
+        lenet_model.eval()
         ds_test = NumericImageFolder(os.path.join(dataset, root), transform=IMG_FOLDER_TRANFORM)
         dl_test = DataLoader(ds_test, batch_size=MNIST_BATCH_SIZE, shuffle = False, num_workers = NUM_DATA_LOADER_WORKERS)
-        predictions = run_predictions(lenet_model, dl_test)
-        evaluate_dataset(model_name, ds_test.targets, predictions, ds_name, root)
+        
+        org_predictions = run_predictions(lenet_model, dl_test)
+        _ = evaluate_dataset(model_name, ds_test.targets, org_predictions, ds_name, root)
 
-    fgsm_attack = torchattacks.FGSM(lenet_model)
+        fgsm_attack = torchattacks.FGSM(lenet_model)
+        atk_predictions = run_predictions(lenet_model, dl_test, fgsm_attack)
+        num_incorrect = evaluate_dataset(model_name, ds_test.targets, atk_predictions, ds_name, root, fgsm_attack.attack)
 
-    for root in tqdm(RECON_ROOT_NAMES):
-        ds_test = NumericImageFolder(os.path.join(dataset, root), transform=IMG_FOLDER_TRANFORM)
-        dl_test = DataLoader(ds_test, batch_size=MNIST_BATCH_SIZE, shuffle = False, num_workers = NUM_DATA_LOADER_WORKERS)
-        predictions = run_predictions(lenet_model, dl_test, fgsm_attack)
-        evaluate_dataset(model_name, ds_test.targets, predictions, ds_name, root, fgsm_attack.attack)
+        if root in ["data_original", "data_recon_4"]:
+            run_tsne(model_name, ds_test, atk_predictions, ds_name, root, num_classes, fgsm_attack.attack)
+            
+            test_idxs, _ = train_test_split(
+                range(len(ds_test)),
+                stratify = ds_test.targets,
+                train_size = num_incorrect,
+                random_state = RANDOM_SEED
+            )
+
+            ds_test = Subset(ds_test, test_idxs)
+            dl_test = DataLoader(ds_test, batch_size = MNIST_BATCH_SIZE, shuffle = False, num_workers = NUM_DATA_LOADER_WORKERS)
+            new_predictions = run_predictions(lenet_model, dl_test)
+            run_tsne(model_name, ds_test, new_predictions, ds_name, root, num_classes, show_incorrect=False)
 
 
-def eval_tiled_model(model_save_path, model_name, dataset):
 
-    lenet_model = torch.load(model_save_path)
-    lenet_model.eval()
+def eval_tiled_model(model_save_path, model_name, dataset, num_classes):
 
     ds_name = dataset.split("\\")[-1]
 
     for i in tqdm(range(len(RECON_ROOT_NAMES) - 1)):
+        lenet_model = torch.load(model_save_path)
+        lenet_model.eval()
         root = RECON_ROOT_NAMES[i]
         ds_test = NumericImageFolder(os.path.join(dataset, root), transform=IMG_FOLDER_TRANFORM)
         ds_test = CombinedDataSet(ds_test, num_tiles=2, tile_split="v")
         dl_test = DataLoader(ds_test, batch_size=MNIST_BATCH_SIZE, shuffle = False, num_workers = NUM_DATA_LOADER_WORKERS)
-        evaluate_dataset(lenet_model, model_name, dl_test, ds_name, root)
+        
+        org_predictions = run_predictions(lenet_model, dl_test)
+        _ = evaluate_dataset(model_name, ds_test._targets, org_predictions, ds_name, root)
 
-    fgsm_attack = torchattacks.FGSM(lenet_model)
+        fgsm_attack = torchattacks.FGSM(lenet_model)
+        atk_predictions = run_predictions(lenet_model, dl_test, fgsm_attack)
+        num_incorrect = evaluate_dataset(model_name, ds_test._targets, atk_predictions, ds_name, root, fgsm_attack.attack)
 
-    for i in tqdm(range(len(RECON_ROOT_NAMES) - 1)):
-        root = RECON_ROOT_NAMES[i]
-        ds_test = NumericImageFolder(os.path.join(dataset, root), transform=IMG_FOLDER_TRANFORM)
-        ds_test = CombinedDataSet(ds_test, num_tiles=2, tile_split="v")
-        dl_test = DataLoader(ds_test, batch_size=MNIST_BATCH_SIZE, shuffle = False, num_workers = NUM_DATA_LOADER_WORKERS)
-        evaluate_dataset(lenet_model, model_name, dl_test, ds_name, root, fgsm_attack)
+        if root in ["data_original", "data_recon_3"]:
+            run_tsne(model_name, ds_test, atk_predictions, ds_name, root, num_classes, fgsm_attack.attack)
+
+            test_idxs, _ = train_test_split(
+                range(len(ds_test)),
+                stratify = ds_test._targets,
+                train_size = num_incorrect, 
+                random_state = RANDOM_SEED
+            )
+
+            ds_test = Subset(ds_test, test_idxs)
+            dl_test = DataLoader(ds_test, batch_size = MNIST_BATCH_SIZE, shuffle = False, num_workers=NUM_DATA_LOADER_WORKERS)
+            new_predictions = run_predictions(lenet_model, dl_test)
+            run_tsne(model_name, ds_test, new_predictions, ds_name, root, num_classes, show_incorrect = False)
+            
+
     
 def main():
 
-    #eval_model(LENET_MNIST_PATH, "Lenet", IMG_MNIST_DIR_PATH)
-    #eval_model(LENET_FASH_MNIST_PATH, "Lenet", IMG_FASH_MNIST_DIR_PATH)
-    #eval_model(LENET_EMNIST_PATH, "Lenet", IMG_EMNIST_DIR_PATH)
+    eval_model(LENET_MNIST_PATH, "Lenet", IMG_MNIST_DIR_PATH, 10)
+    eval_model(LENET_FASH_MNIST_PATH, "Lenet", IMG_FASH_MNIST_DIR_PATH, 10)
+    eval_model(LENET_EMNIST_PATH, "Lenet", IMG_EMNIST_DIR_PATH, 47)
 
-    #eval_tiled_model(LENET_MNIST_PATH, "Lenet", IMG_TILED_MNIST_DIR_PATH)
-    #eval_tiled_model(LENET_FASH_MNIST_PATH, "Lenet", IMG_TILED_FASH_MNIST_DIR_PATH)
-    #eval_tiled_model(LENET_EMNIST_PATH, "Lenet", IMG_TILED_EMNIST_DIR_PATH)
+    eval_tiled_model(LENET_MNIST_PATH, "Lenet", IMG_TILED_MNIST_DIR_PATH, 10)
+    eval_tiled_model(LENET_FASH_MNIST_PATH, "Lenet", IMG_TILED_FASH_MNIST_DIR_PATH, 10)
+    eval_tiled_model(LENET_EMNIST_PATH, "Lenet", IMG_TILED_EMNIST_DIR_PATH, 47)
 
-    eval_model(LENET_MNIST_PATH, "Lenet", IMG_MNIST_GELU_DIR_PATH)
+    eval_model(LENET_MNIST_PATH, "Lenet", IMG_MNIST_GELU_DIR_PATH, 10)
 
 
 if __name__ == "__main__": 
--- proposal_visuals.py
+++ proposal_visuals.py
@@ -42,99 +42,12 @@ def run_tsne(model_name, ds_test, predictions, ds_name, recon_name, num_classes,
     ds_version = "_".join(recon_name.split("_")[1:])
     title = f"{model_name}'s {points_shown} points on {ds_name} {ds_version}"
     if attack is not None: 
-        title = f"{model_name}'s {points_shown} points on {ds_name} {ds_version} with {attack.attack} Attack"
+        title = f"{model_name}'s {points_shown} points on {ds_name} {ds_version} with {attack} Attack"
     plt.title(title)
     file_name = f"TSNE_{model_name}_{points_shown}_{ds_name}_{ds_version}.png"
     if attack is not None:
-        file_name = f"TSNE_{model_name}_{points_shown}_{ds_name}_{ds_version}_{attack.attack}.png"
+        file_name = f"TSNE_{model_name}_{points_shown}_{ds_name}_{ds_version}_{attack}.png"
     plt.legend(bbox_to_anchor=(1.01, 1), loc='upper left', borderaxespad=0)
     plt.tight_layout()
     plt.savefig(os.path.join(VISUAL_DIR, file_name))
     plt.clf()
-
-    num_incorrect = len(points_to_show) if show_incorrect else len(ds_test) - len(points_shown)
-    return num_incorrect
-
-
-def run_model(model_save_path, model_name, dataset, root, num_classes, run_attack = False, show_incorrect = True):
-    lenet_model = torch.load(model_save_path)
-    lenet_model.eval()
-    
-    ds_name = dataset.split("\\")[-1]
-
-    attack = None
-    if run_attack:
-        attack = torchattacks.FGSM(lenet_model)
-
-    ds_test = NumericImageFolder(os.path.join(dataset, root), transform=IMG_FOLDER_TRANFORM)
-    dl_test = DataLoader(ds_test, batch_size=MNIST_BATCH_SIZE, shuffle = False, num_workers = NUM_DATA_LOADER_WORKERS)
-    predictions = run_predictions(lenet_model, dl_test, attack)
-    run_tsne(model_name, ds_test, predictions, ds_name, root, num_classes, attack, show_incorrect)
-        
-
-if __name__ == "__main__":
-    roots = ["data_original", "data_recon_4"]
-
-    # running through generic TSNE visualizations showing misclassified points with true values
-    for root in roots:
-        run_model(LENET_MNIST_PATH, "Lenet", IMG_MNIST_DIR_PATH, root, 10)
-        run_model(LENET_FASH_MNIST_PATH, "Lenet", IMG_FASH_MNIST_DIR_PATH, root, 10)
-        run_model(LENET_EMNIST_PATH, "Lenet", IMG_EMNIST_DIR_PATH, root, 47)
-
-        run_model(LENET_MNIST_PATH, "Lenet", IMG_MNIST_DIR_PATH, root, 10, run_attack = True)
-        run_model(LENET_FASH_MNIST_PATH, "Lenet", IMG_FASH_MNIST_DIR_PATH, root, 10, run_attack = True)
-        run_model(LENET_EMNIST_PATH, "Lenet", IMG_EMNIST_DIR_PATH, root, 47, run_attack = True) 
-
-    # more specific visualizations
-    mnist_original_fgsm_num_incorrect = 374
-    ds_test = NumericImageFolder(os.path.join(IMG_MNIST_DIR_PATH, "data_original"), transform = IMG_FOLDER_TRANFORM)
-    test_idxs, _ = train_test_split(
-        range(len(ds_test)),
-        stratify=ds_test.targets,
-        train_size = mnist_original_fgsm_num_incorrect,
-        random_state = 42
-    )
-
-    ds_test = Subset(ds_test, test_idxs)
-    dl_test = DataLoader(ds_test, batch_size=MNIST_BATCH_SIZE, shuffle = False, num_workers=NUM_DATA_LOADER_WORKERS)
-    lenet_model = torch.load(LENET_MNIST_PATH)
-    ds_name = "MNIST"
-    predictions = run_predictions(lenet_model, dl_test)
-    run_tsne("Lenet", ds_test, predictions, ds_name, "data_original", 10, None, show_incorrect=False)
-
-
-    mnist_recon_4_fgsm_num_incorrect = 2200
-    ds_test = NumericImageFolder(os.path.join(IMG_MNIST_DIR_PATH, "data_original"), transform = IMG_FOLDER_TRANFORM)
-    test_idxs, _ = train_test_split(
-        range(len(ds_test)),
-        stratify=ds_test.targets,
-        train_size = mnist_recon_4_fgsm_num_incorrect,
-        random_state = 42
-    )
-
-    ds_test = Subset(ds_test, test_idxs)
-    dl_test = DataLoader(ds_test, batch_size=MNIST_BATCH_SIZE, shuffle = False, num_workers=NUM_DATA_LOADER_WORKERS)
-    lenet_model = torch.load(LENET_MNIST_PATH)
-    ds_name = "MNIST"
-    predictions = run_predictions(lenet_model, dl_test)
-    run_tsne("Lenet", ds_test, predictions, ds_name, "data_recon_4", 10, None, show_incorrect=False)
-
-
-    lenet_model = torch.load(LENET_MNIST_PATH)
-    attack = torchattacks.FGSM(lenet_model)
-    ds_test = NumericImageFolder(os.path.join(IMG_TILED_MNIST_DIR_PATH, "data_recon_3"), transform = IMG_FOLDER_TRANFORM)
-    ds_test = CombinedDataSet(ds_test, num_tiles=2, tile_split="v")
-    dl_test = DataLoader(ds_test, batch_size=MNIST_BATCH_SIZE, shuffle=False, num_workers=NUM_DATA_LOADER_WORKERS)
-    predictions = run_predictions(lenet_model, dl_test, attack)
-    num_incorrect = run_tsne("Lenet", ds_test, predictions, "Tiled_MNIST", "data_recon_3", 10, attack, show_incorrect = True)
-    test_idxs, _ = train_test_split(
-        range(len(ds_test)),
-        stratify = ds_test._targets,
-        train_size = num_incorrect,
-        random_state = 42 
-    )
-
-    ds_test = Subset(ds_test, test_idxs)
-    dl_test = DataLoader(ds_test, batch_size = MNIST_BATCH_SIZE, shuffle = False, num_workers = NUM_DATA_LOADER_WORKERS)
-    predictions = run_predictions(lenet_model, dl_test)
-    _ = run_tsne("Lenet", ds_test, predictions, "Tiled_MNIST", "data_recon_3", 10, None, show_incorrect = False)
