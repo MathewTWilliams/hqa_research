f02a1fde103fd2311d6187dc17611af9f1429039
Matt Williams <williams.mathew.t@gmail.com>
2023-04-09 23:06:09 -0400

    small change

--- visualize_wasserstein.py
+++ visualize_wasserstein.py
@@ -44,12 +44,12 @@ def make_result_distributions():
             fig.set_figwidth(10)
             fig.set_figheight(8)
             
-            sns.histplot(ax=axes[0], data = cor_wass_org_img_df, x = "Wasserstein_Distance", kde=True, stat="proportion", bins=25)
+            sns.histplot(ax=axes[0], data = cor_wass_org_img_df, x = "Wasserstein_Distance", kde=True, stat="density", bins=25)
             axes[0].set_title(f"Wasserstein distances of classified {label} images produced by {recon}: {len(cor_wass_org_img_df.index)}", {"size": 9})
             axes[0].set_xlim(0,2.5)
             axes[0].set_ylim(0,0.4)
 
-            sns.histplot(ax=axes[1], data = inc_wass_atk_img_df, x = "Wasserstein_Distance", kde=True, stat="proportion", bins=25)
+            sns.histplot(ax=axes[1], data = inc_wass_atk_img_df, x = "Wasserstein_Distance", kde=True, stat="density", bins=25)
             axes[1].set_title(f"Wasserstein distance of attacked images misclassified as {label} produced by {recon}: {len(inc_wass_atk_img_df.index)}", {"size": 9})
             axes[1].set_xlim(0,2.5)
             axes[1].set_ylim(0,0.4)
